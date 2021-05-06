57a860aed149ee6cf41a938ad6cc4b168d1ca8ff
lufeng <lufengbelove@qq.com>
2021-05-06 16:13:31 +0800

    init decoo-wiki repo

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require("/Users/lf/decoo/decoo-wiki/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/lf/decoo/decoo-wiki/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/lf/decoo/decoo-wiki/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/lf/decoo/decoo-wiki/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/lf/decoo/decoo-wiki/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/lf/decoo/decoo-wiki/src/css/custom.css"),
+];
