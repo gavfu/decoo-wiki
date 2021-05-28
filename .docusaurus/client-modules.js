39427bf90cca4b540ec8034145df147eb642be40
Gavin Fu <gavfu@outlook.com>
2021-05-28 11:37:34 +0800

    Fix build error and remove .docusaurus directory

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/Users/gavfu/github/decooio/decoo-wiki/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/gavfu/github/decooio/decoo-wiki/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/gavfu/github/decooio/decoo-wiki/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/gavfu/github/decooio/decoo-wiki/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/gavfu/github/decooio/decoo-wiki/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/gavfu/github/decooio/decoo-wiki/src/css/custom.css"),
-];
